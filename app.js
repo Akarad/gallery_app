e84633260e054aa52476e54bbc9e92df67379b0e
Akarad <dianaakara@gmail.com>
2020-07-27 21:41:41 +0300

    add .gitignore,app.js and json files

---
+++ app.js
@@ -0,0 +1,19 @@
+// Import express framework
+const express = require('express');
+
+// Initialize express
+const app = express();
+
+// route for the index page
+app.get('/', (req, res)=>{
+    response.send('<h1> Welcome to my app</h1>')
+ });
+ 
+
+// Define the port number
+const PORT = 5000;
+
+
+app.listen(PORT, function(){
+   console.log(`Server is listening on port ${PORT}`)
+})
