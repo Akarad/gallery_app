c3ec0a49119fa8ef0d1aa40e872ab657d69f280a
Akarad <dianaakara@gmail.com>
2020-08-03 19:06:27 +0300

    add elements to app.js file,renamed stylesheet and added code to index.html

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-// Import express framework
+// Import express package
 const express = require('express');
+
 
 // Initialize express
 const app = express();
@@ -7,16 +8,9 @@ const app = express();
 // Set a static folder
 app.use(express.static('public'));
 
-// route for the index page
-app.get('/', (req, res)=>{
-    response.send('<h1> Welcome to my app</h1>')
- });
- 
-
 // Define the port number
 const PORT = 5000;
 
-
-app.listen(PORT, function(){
+app.listen(PORT, ()=>{
    console.log(`Server is listening on port ${PORT}`)
 })
