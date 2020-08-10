698619e422ed0caa257948be2d137e7742b04c16
Akarad <dianaakara@gmail.com>
2020-08-10 20:08:57 +0300

    add route to handle image upload

--- app.js
+++ app.js
@@ -16,6 +16,18 @@ app.get('/', (req,res)=>{
    res.render('index');
 })
 
+// route to handle image upload
+app.post('/upload', (req,res)=>{
+   upload(req,res, (err)=>{
+       if (err){
+           console.log(err)
+       }else{
+           console.log(req.file)
+           res.send('test');
+       }
+   })
+})
+
 // Define the port number
 const PORT = 5000;
 
